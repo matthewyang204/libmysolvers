1fd41bcd3cf84d9cf3a600740394c3ba1ba87835
Matthew Yang <matthew.yang204@gmail.com>
2025-07-15 18:09:12 -0700

    Add function signature declarations in .h file

---
+++ libmysolvers.h
@@ -0,0 +1,7 @@
+#ifndef LIBMYSOLVERS_H
+#define LIBMYSOLVERS_H
+
+double quadratic_single_neg_solver(double a, double b, double c);
+double quadratic_single_pos_solver(double a, double b, double c);
+
+#endif
