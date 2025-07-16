7b82eab7b2e657cf403ab5d29fd15d0f7033fc3f
Matthew Yang <matthew.yang204@gmail.com>
2025-07-15 18:07:07 -0700

    Write negative and positive solvers

---
+++ libmysolvers.c
@@ -0,0 +1,36 @@
+#include libmysolvers.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double quadratic_single_neg_solver(double a, double b, double c) {
+    if (a == 0) {
+        fprintf(stderr, "Coefficient 'a' cannot be zero in a quadratic equation, or else it will not be an equation at all.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    double discriminant = (b * b) - (4 * a * c);
+    if (discriminant < 0) {
+        fprintf(stderr, "The equation has no real roots.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    double x = (-b - sqrt(discriminant)) / (2 * a);
+    return x;
+}
+
+double quadratic_single_pos_solver(double a, double b, double c) {
+    if (a == 0) {
+        fprintf(stderr, "Coefficient 'a' cannot be zero in a quadratic equation, or else it will not be an equation at all.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    double discriminant = (b * b) - (4 * a * c);
+    if (discriminant < 0) {
+        fprintf(stderr, "The equation has no real roots.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    double x = (-b + sqrt(discriminant)) / (2 * a);
+    return x;
+}
