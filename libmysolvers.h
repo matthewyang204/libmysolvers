65878a1a4a60ea6e5daf2ccf1e21cc7a267a037d
Matthew Yang <matthew.yang204@gmail.com>
2025-07-17 17:16:49 -0700

    Add licensing headers

--- libmysolvers.h
+++ libmysolvers.h
@@ -1,3 +1,7 @@
+// Copyright 2025 Matthew Yang
+// This file is part of libmysolvers.
+// libmysolvers is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
 #ifndef LIBMYSOLVERS_H
 #define LIBMYSOLVERS_H
 
