fbfaa0409123405b12c19253ebc633a0d28d405b
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-11-20 19:59:01 +0000

    Link to libcharge to its corresponding header file

--- libcharge.c
+++ libcharge.c
@@ -2,3 +2,4 @@
 // This file is part of libcharge, which is itself a part of libmysolvers.
 // libmysolvers is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
+#include "libcharge.h"
